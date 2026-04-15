80d7a61f291fa2d7ea8331f4f4609f5d0f751eb1
25730144 Đàm Gia Thái <25730144@ms.uit.edu.vn>
2026-04-16 00:12:37 +0700

    an moi

--- main.cpp
+++ main.cpp
@@ -48,6 +48,10 @@ int main()
             if (t=='d') Huong = 0;
             if (t=='x') Huong = 1;
         }
+		
+		//tesest an moi
+		Sleep(3);
+		
         system("cls");
         r.Ve();
         r.DiChuyen(Huong);
